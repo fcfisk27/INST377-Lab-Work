78eccdcac3365bd8e2c957d9663a30215b22ac21
fcfisk27 <74329174+fcfisk27@users.noreply.github.com>
2022-02-22 15:18:07 -0500

    Update script.js

---
+++ client/tetris/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = ArrayFrom(document.querySelectorAll('.grid div'))
+  const width = 10
+  console.log(squares);
+});

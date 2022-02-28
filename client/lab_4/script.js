243c6d13600e46a14f06b9b9a19e4d9ee4de742a
fcfisk27 <74329174+fcfisk27@users.noreply.github.com>
2022-02-28 18:50:03 -0500

    lab 4

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,4 +1,42 @@
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', function(event){
-    console.log('Button Clicked');
-})
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel_item');
+const totalSlides = slides.length;
+
+document.
+    querySelector('#carousel_button--next')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
+document.
+    querySelector('#carousel_button--prev')
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('visible-item');
+        slide.classList.add('hidden-item')
+    }
+
+    slides[slidePosition].classList.add('visible-item')
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = 0;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
